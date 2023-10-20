255dbfae0d61f19cb657b24449ad71d7905a65bf
Yuda <115441892+yydaaa@users.noreply.github.com>
2023-10-20 15:36:48 +0700

    Add files via upload

---
+++ pages/index.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+  <table border="1" width="50%">
+    <thead>
+      <tr>
+        <th>no</th>
+        <th>tanggal</th>
+        <th>nama</th>
+        <th>kategori</th>
+        <th>keperluan</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="(kunjungan,index) in datas" :key="kunjungan.id">
+        <td>{{ index + 1 }}</td>
+        <td>{{ kunjungan.tanggal }}</td>
+        <td>{{ kunjungan.nama }}</td>
+        <td>{{ kunjungan.kategori }}</td>
+        <td>{{ kunjungan.keperluan }}</td>
+      
+
+    </tr>
+    </tbody>
+  </table>
+  </div>
+
+</template>
+
+<script setup>
+const supabase = useSupabaseClient()
+const datas = ref([])
+
+async function getData(){
+  const{data,error}=await supabase
+  .from("kunjungan")
+  .select()
+  if (error) throw
+  if (data){
+      console.log(data)
+      datas.value=data
+  }
+}
+
+onMounted(() =>{
+  getData()
+})
+</script>
+<style scope>
+
+</style>
