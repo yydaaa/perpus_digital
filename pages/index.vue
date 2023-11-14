406f8e8d26c789e311ac31831809a4d7a467d120
Yuda <115441892+yydaaa@users.noreply.github.com>
2023-11-14 15:39:16 +0700

    index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,30 +1,56 @@
 <template>
-  <div>
-  <table border="1" width="50%">
-    <thead>
-      <tr>
-        <th>no</th>
-        <th>tanggal</th>
-        <th>nama</th>
-        <th>kategori</th>
-        <th>keperluan</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr v-for="(kunjungan,index) in datas" :key="kunjungan.id">
-        <td>{{ index + 1 }}</td>
-        <td>{{ kunjungan.tanggal }}</td>
-        <td>{{ kunjungan.nama }}</td>
-        <td>{{ kunjungan.kategori }}</td>
-        <td>{{ kunjungan.keperluan }}</td>
-      
+  <!--Input Data For DataBase-->
+  <div class="content">
+      <h3>Isi Data <br>Pengunjung</h3>
+      <form @submit.prevent="addData" >
+          <div class="row">
+              <div class="col-sm-10">
+                  <input v-model="name" type="text" class="form-control form-control-lg" id="name" placeholder="Nama..." required>
+              </div>
+            </div>
 
-    </tr>
-    </tbody>
-  </table>
+          <div class="row">
+              <div class="col-sm-10">
+                  <input v-model="kategori" type="text" class="form-control form-control-lg" id="kategori" placeholder="Kategori..." required>
+              </div>
+          </div>
+
+          <div class="row">
+              <div class="col-sm-10">
+                  <input v-model="keperluan" type="text" class="form-control form-control-lg" id="keperluan" placeholder="Keperluan..." required>
+              </div>
+          </div>  
+          <button class="btn">Submit</button>
+      </form>
   </div>
 
+  <!--Showing Data Tables-->
+  <div class="showData">
+      <h1>Riwayat Pengunjung</h1>
+      <div class="table-responsive">
+          <div class="m-2">Menampilkan {{ datas.length }} data</div>
+          <table class="table table-responsive">
+              <thead>
+                  <tr>
+                      <th class="no">No</th>
+                      <th class="nama">Nama</th>
+                      <th class="kategori">Kategori</th>
+                      <th class="keperluan">Keperluan</th>
+                      <th class="tanggal">Tanggal</th>
+              </tr>
+          </thead>
+          <tbody v-for="(kunjungan,index) in datas" :key="kunjungan.id ">
+              <td>{{ index + 1 }}</td>
+              <td>{{ kunjungan.nama }}</td>
+              <td>{{ kunjungan.kategori }}</td>
+              <td>{{ kunjungan.keperluan }}</td>
+              <td>{{ kunjungan.tanggal }}</td>
+          </tbody>
+      </table>
+  </div>
+  </div>
 </template>
+
 
 <script setup>
 const supabase = useSupabaseClient()
@@ -34,13 +60,24 @@ async function getData(){
   const{data,error}=await supabase
   .from("kunjungan")
   .select()
-  if (error) throw
+  if (error) throw error
   if (data){
-      console.log(data)
+    console.log(data)
       datas.value=data
   }
 }
-
+// Add Data From Input To Database
+async function addData(){
+    const { error } = await supabase
+    .from('anggota')
+    .insert([{
+        kategori: kategori.value,
+        nama: name.value,
+        keperluan: keperluan.value
+    }])
+    if (error) throw error
+    else getData()
+}
 onMounted(() =>{
   getData()
 })
