5c5d7fb69d59821e29ffcdd4ba72c28a5ab905bd
Yuda <115441892+yydaaa@users.noreply.github.com>
2023-11-17 01:13:04 +0700

    header.vue

--- componets/header.vue
+++ componets/header.vue
@@ -1,12 +1,11 @@
 <template>
     <div>
         <div class="nav-container">
-            <img src="@/assets/images/logosmk.png" class="logosmk"/>
+            <img src="@/assets/images/smk.png" class="smk"/>
             <div class="headerText">
-                <h1>PERPUS DIGIAL
-                    SMKN 4 TASIKMALAYA</h1>
+            <h1>PERPUS-DIGITAL SMKN-4 TASIKMALAYA</h1>
+            
             </div>
-            <h4>KAMBING TUA</h4>
             <img src="@/assets/images/logoclub.png" class="logoclub"/>
                            
 
@@ -22,11 +21,11 @@
 *{
 margin: 0;
 z-index: 1;
-}
+    }
 .nav-container {
-    background-color:#FF9900;
+    background-color:rgba(255, 162, 0, 0.678);
     width: 100%;
-    height: 150px;
+    height: 90px;
     display: flex;
     justify-content: space-evenly;
     align-items: center;
@@ -37,14 +36,13 @@ z-index: 1;
 }
 
 .logoclub {
-    width: 100px;
-    height: 90px;
+    width: 150px;
+    height: 130px;
 }
 
-.logosmk{
-    width: 111px;
-    height: 126px;
-
+.smk{
+    width: 120px;
+    height: 110px;
 }
 h1 {
     color: #ffffff;
@@ -57,64 +55,12 @@ h1 {
 
 }
 
-h4{
-    color: #ffffff;
-    text-align: right;
-    font-size: 50px;
-    letter-spacing: 4px;
-    font-weight: 60;
-}
 
 @media (max-width: 520px) {
     .nav-container {
-        background-color:#FF9900;
+        background-color:rgbargba(255, 162, 0, 0.678);
         width: 100%;
         height: 80px;
-        display: flex;
-        justify-content: space-evenly;
-        align-items: center;
-        position: fixed;
-        top: 0;
-        left: 0;
-        right: 0;
-    }
-    
-    .logosmk {
-        width: 150px;
-        height:100px;
-    }
-    
-    }
-    .logosmk{
-        width: 46.077px;
-        height: 50px;
-    
-    }
-    h1 {
-        color: #ffffff;
-        text-align: right;
-        font-family: '';
-        font-size: 26px;
-        letter-spacing: 4px;
-        font-style: 'bold';
-    
-    }
-    
-    h4{
-        color: #ffffff;
-        text-align: center;
-        font-family: 'Charm', cursive;
-        font-size: 35px;
-        letter-spacing: 2px;
-        font-style: 'bold';
-        font-weight: 60;
-    }
-
-@media (max-width: 820px) {
-    .nav-container {
-        background-color:#FF9900;
-        width: 100%;
-        height: 130px;
         display: flex;
         justify-content: space-evenly;
         align-items: center;
@@ -125,32 +71,55 @@ h4{
     }
     
     .logoclub {
-        width: 100px;
-        height:118px;
+        width: 130px;
+        height: 110px;
     }
     
-    
-    .logosmk{
-        width: 80.077px;
+    }
+    .smk{
+        width: 80px;
         height: 80px;
     
     }
     h1 {
-        color: #EFDE79;
+        color: #ffffff;
+        text-align: right;
+        font-size: 19px;
+        letter-spacing: 1px;
+    
+    }
+    
+@media (max-width: 820px) {
+    .nav-container {
+        background-color:rgba(255, 162, 0, 0.678);
+        width: 100%;
+        height: 90px;
+        display: flex;
+        justify-content: space-evenly;
+        align-items: center;
+        position: fixed;
+        top: 0;
+        left: 0;
+        right: 0;
+    }
+    
+    .logoclub {
+        width: 120px;
+        height:120px;
+    }
+    
+    
+    .smk{
+        width: 80px;
+        height: 80px;
+    
+    }
+    h1 {
+        color: #ffffff;
         text-align: center;
-        font-family: '';
         font-size: 15px;
-        letter-spacing: 2px;
-        font-style: 'bold';
-    
+        letter-spacing: 1px;
     }
-    
-    h4{
-        color: #fff9c2;
-        text-align: left;
-        font-family: 'Charm', cursive;
-        font-size: 12px;
-        font-weight: 60;
-    }
+
 }
 </style>
